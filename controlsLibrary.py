b761c122e56dde9e7a55f51f8de911f979521e49
alvaroCano-IKFK <acpradillo@gmail.com>
2026-05-14 19:34:20 +0200

    changed path in control library to my host path(se me olvidó xd)

--- controlsLibrary.py
+++ controlsLibrary.py
@@ -5,7 +5,7 @@ import os
 import json
 
 # --- CONFIGURACIÓN DE PATHS ---
-BASE_PATH = r"C:\Users\Usuario\Documents\maya\scripts"
+BASE_PATH = r"C:\Users\Usuario\Documents\GitHub\La-monja-gitana-autorig"
 CONTROLS_DIR = os.path.join(BASE_PATH, "control_library")
 
 # Asegurar que la carpeta existe

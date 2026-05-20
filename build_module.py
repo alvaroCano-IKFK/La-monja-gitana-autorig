854a79c695fbf0a78d094a501ae0742ba067d16b
alvaroCano-IKFK <acpradillo@gmail.com>
2026-05-20 20:20:25 +0200

    body in build module 2nd round

--- build_module.py
+++ build_module.py
@@ -19,6 +19,7 @@ import right_leg_module
 import skinning_module
 import guides_module
 import build_module
+import body_module
 
 class BuildRig(object):
     
@@ -124,5 +125,13 @@ class BuildRig(object):
                 root_instance=self.root_rig
             )
         skn.build()
-                        
+
+        # Construir el cuerpo (body_module)
+        self.body_rig = body_module.BodyModule(
+            root_guide="root",
+            rig_name="Character",
+            root_instance=self.root_rig
+        )
+        self.body_rig.build()
+
         print("Build completo: Spine, Arm y Leg construidos desde build_module.")

9b5a0274e76f94f5f1281bce2e3afdb298507751
alvaroCano-IKFK <acpradillo@gmail.com>
2026-05-20 20:32:37 +0200

    indentacion

--- body_module.py
+++ body_module.py
@@ -9,26 +9,27 @@ class BodyModule(object):
                  rig_name="Character", 
                  root_instance=None):
         
-        self.root_guide = root_guide
-        self.rig_name  = rig_name
+        self.root_guide= root_guide
+        self.rig_name= rig_name
 
         self.ctrl_style = "bodyControl"
 
-        self.group_maker   = groups_module.ControlsGroups()
-        self.root_instance = root_instance
+        self.group_maker= groups_module.ControlsGroups()
+        self.root_instance= root_instance
         
 
-def build(self):
-    pos_spine = cmds.xform(self.root_guide, q=True, ws=True, t=True)
-    cmds.select(clear=True)
-    body_joint = cmds.joint(n=f"{self.rig_name}_body_JNT", p=pos_spine)
+    def build(self):
 
-    #Control
-    name = f"{self.rig_name}_body_CTL"
-    bodyControl = controlsLibrary.create_control_from_lib(
-        lib_name=self.ctrl_style, 
-        final_name=name)
-    bodyControl_off = self.group_maker.create_rig_hierarchy(bodyControl, self.root_guide)
-    cmds.parentConstraint(bodyControl, body_joint)
+        pos_spine = cmds.xform(self.root_guide, q=True, ws=True, t=True)
+        cmds.select(clear=True)
+        body_joint = cmds.joint(n=f"{self.rig_name}_body_JNT", p=pos_spine)
 
-    print("Body module built successfully.")
+        #Control
+        name = f"{self.rig_name}_body_CTL"
+        bodyControl = controlsLibrary.create_control_from_lib(
+            lib_name=self.ctrl_style, 
+            final_name=name)
+        bodyControl_off = self.group_maker.create_rig_hierarchy(bodyControl, self.root_guide)
+        cmds.parentConstraint(bodyControl, body_joint)
+
+        print("Body module built successfully.")

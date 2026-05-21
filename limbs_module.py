872e2e25e603d5c748493d8a5f0cfb00bd058bbc
alvaroCano-IKFK <acpradillo@gmail.com>
2026-05-21 20:12:37 +0200

    new try on list

--- limbs_module.py
+++ limbs_module.py
@@ -168,19 +168,12 @@ class LimbModule(object):
             final_name=f"{self.prefix}_clavicule_CTRL"
         )
         clav_gen = self.group_maker.create_rig_hierarchy(clavicule_ctl, self.clavicule_guide)
-        
         if self.side == "R":
-            if isinstance(clav_gen, list):
-                target_group = clav_gen[0]
-            else:
-                target_group = clav_gen
+            mirror_behavior_grp = f"{self.root_instance.rig_name}_mirrorBehaviour_GRP"
+            cmds.setAttr(f"{clav_gen[0]}.scaleZ", 1)
 
-            # Aplicamos el setAttr INMEDIATAMENTE aquí, antes de moverlo de sitio en el Outliner
-            if cmds.objExists(target_group):
-                cmds.setAttr(f"{target_group}.scaleZ", 1)
-                print(f"--> Escala Z forzada con éxito en: {target_group}")
-            # ---------------------
-        
+            cmds.parent(clav_gen, mirror_behavior_grp)
+            
         cmds.matchTransform(clav_gen, self.clavicule_guide)
         cmds.parentConstraint(clavicule_ctl, b_cl, mo=True)
         #cmds.parent(clav_gen, self.controls_grp)

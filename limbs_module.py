3d0521a87ab03647c0fbb63141c6c0fc36856afd
alvaroCano-IKFK <acpradillo@gmail.com>
2026-05-21 19:54:00 +0200

    new order

--- limbs_module.py
+++ limbs_module.py
@@ -170,8 +170,8 @@ class LimbModule(object):
         clav_gen = self.group_maker.create_rig_hierarchy(clavicule_ctl, self.clavicule_guide)
         if self.side == "R":
             mirror_behavior_grp = f"{self.root_instance.rig_name}_mirrorBehaviour_GRP"
-            cmds.setAttr(f"{clav_gen}.scaleZ", 1)
             cmds.parent(clav_gen, mirror_behavior_grp)
+            cmds.setAttr(f"{clav_gen}.scaleZ", 1)
             
         cmds.matchTransform(clav_gen, self.clavicule_guide)
         cmds.parentConstraint(clavicule_ctl, b_cl, mo=True)

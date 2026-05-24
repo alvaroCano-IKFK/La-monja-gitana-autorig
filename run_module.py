e1a96212ff96f306dd909277410e0efe5f16c883
alvaroCano-IKFK <acpradillo@gmail.com>
2026-05-24 13:56:10 +0200

    test

--- run_module.py
+++ run_module.py
@@ -1,7 +1,7 @@
 import master_module
 import importlib
 importlib.reload(master_module)
-print("Test:This is an opened repository, not a cloned one. ")
+print("Test:This is an opened repository. No funciona bien. ")
 
 
 master_module.run()

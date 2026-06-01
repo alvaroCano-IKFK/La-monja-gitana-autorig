cd171dbc8a2faa42da5eb9936d4cfe03e2e8ec5b
laiavila4 <laiavilagalera@gmail.com>
2026-06-01 11:36:48 +0200

    a dios le pido

--- twist_module.py
+++ twist_module.py
@@ -28,7 +28,14 @@ class TwistModule(object):
 
         self.base_curve = cmds.curve(degree =1, bezier=2, p=[(pos_start_joint, pos_mid_joint, pos_end_joint)], knot=[0, 1])
 
-        detatch_result = cmds.detachCurve("{}.u[0.5]".format(self.base_curve), ch=True, ko=True)
+        detatch_result = cmds.detachCurve((f"{self.base_curve}.u[0.5]"), ch=True, ko=True)
+
+        self.upper_curve = cmds.rename(detatch_result[0], f"{self.name}UpperSegment_CRV")
+        self.lower_curve = cmds.rename(detatch_result[1], f"{self.name}LowerSegment_CRV")
+
+        history = cmds.listHistory(self.upper_curve)
+        node_detach = cmds.ls(history, type="detachCurve")[0]
+        cmds.setAttr(f"{node_detach}.parameter[0]", 0.5)
 
 twist=TwistModule("arm", "L")
 twist.create_basic_curve()

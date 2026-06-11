910e9ea9ba335bb3fb00fe78298924144045fdb1
laiavila4 <laiavilagalera@gmail.com>
2026-06-11 12:21:33 +0200

    nou modul per les cames de radera

---
+++ control_library/back_leg_module.py
@@ -0,0 +1,16 @@
+import maya.cmds as cmds
+import math
+import leg_module
+import controlsLibrary 
+import guides_module
+from groups_module import ControlsGroups
+import rigRoot_module
+import nodeCreator_module
+import hip_module
+from nodeCreator_module import NodeCreator
+import curvature_module
+import twist_module
+import chest_module
+
+class BackLeg(object): 
+    def __init__(self, ):

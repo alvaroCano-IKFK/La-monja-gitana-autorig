ba5cc4c314ae4929f9a8084d9d3f5ec03c05eb75
alvaroCano-IKFK <acpradillo@gmail.com>
2026-09-23 14:51:09 +0200

    xd

--- run_module.py
+++ run_module.py
@@ -1,6 +1,6 @@
 import master_module
 import importlib
 importlib.reload(master_module)
-print("Hola caracola, eres una amapola?")
+print("Hola caracola, eres una amapola?Puede")
 
 master_module.run()
